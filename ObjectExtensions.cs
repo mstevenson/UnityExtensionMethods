b06522c1401af1e66a6721af068884f36b2f3c50
Michael Stevenson <michael@mstevenson.net>
2012-04-08 09:51:31 -0700

    Added FindObjectsOfType<T>

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -6,7 +6,12 @@ public static class ObjectExtensions {
 	public static T FindObjectOfType<T> (this UnityEngine.Object obj)
 		where T : UnityEngine.Object
 	{
-		return (UnityEngine.Object.FindObjectOfType(typeof(T)) as T);
+		return (UnityEngine.Object.FindObjectOfType (typeof(T)) as T);
 	}
 	
+	public static T[] FindObjectsOfType<T> (this UnityEngine.Object obj)
+		where T : UnityEngine.Object
+	{
+		return (UnityEngine.Object.FindObjectsOfType (typeof(T)) as T[]);
+	}
 }
