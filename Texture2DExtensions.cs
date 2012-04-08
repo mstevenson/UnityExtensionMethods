c9061197531450664640bc38eb04795d254ba5e6
Michael Stevenson <michael@mstevenson.net>
2012-04-08 09:51:52 -0700

    Added CreateSharedMaterial extension method to Texture2D

--- Texture2DExtensions.cs
+++ Texture2DExtensions.cs
@@ -11,4 +11,27 @@ public static class Texture2DExtensions
 		GUI.DrawTexture (new Rect (xOffset, yOffset, tex.width, tex.height), tex);
 	}
 	
+	
+	/// <summary>
+	/// Create a basic material from this texture. Any materials created with this
+	/// method from the same texture will be shared and can be batched.
+	/// </summary>
+	public static Material CreateSharedMaterial (this Texture2D texture, string shader)
+	{
+		Shader targetShader = Shader.Find (shader);
+		Material[] mats = Object.FindObjectsOfType (typeof(Material)) as Material[];
+		// Return existing material
+		foreach (var mat in mats) {
+			if (mat.shader != targetShader)
+				continue;
+			if (mat.mainTexture == null)
+				continue;
+			if (mat.mainTexture == texture)
+				return mat;
+		}
+		// Create new material
+		Material newMat = new Material (targetShader);
+		newMat.mainTexture = texture;
+		return newMat;
+	}
 }
